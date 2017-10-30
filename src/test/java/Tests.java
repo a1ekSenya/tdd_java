5e79aa581478776db333c65d0e54a721b837f9d3
Алексей Кононов <aleksej.kononov@academmedia.com>
2017-10-30 13:23:34 +0700

    13

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -5,8 +5,12 @@ import static org.junit.Assert.*;
 public class Tests {
     @Test
     public void testSimpleAddition() {
-        Money sum = Money.dollar(5).plus(Money.dollar(5));
-        assertEquals(Money.dollar(10), sum);
+        Money five = Money.dollar(5);
+        Expression sum = five.plus(five);
+        Bank bank = new Bank();
+        Money reduced = bank.reduce(sum, "USB");
+        assertEquals(Money.dollar(10), reduced);
+
     }
 
     @Test
